5f067095dba2329cd32b66956e6fb4738b9ff58c
Alexandre Beltzac <beltzac@gmail.com>
2013-12-03 23:49:24 -0200

    comparable

--- src/projetobanco/Cliente.java
+++ src/projetobanco/Cliente.java
@@ -4,12 +4,12 @@
  */
 package projetobanco;
 
-
 /**
  *
  * @author Codification
  */
-public class Cliente implements Comparable{
+public class Cliente implements Comparable<Cliente> {
+
     private int codigo;
     private String Nome;
     private String sobrenome;
@@ -18,8 +18,21 @@ public class Cliente implements Comparable{
     private String Endereco;
     private double salario;
 
-   
-            
+    // Which field should be considered for sorting
+    public enum campoOrdenacao {
+
+        nome, sobrenome, salario
+    }
+
+    // Sorting direction ASCENDING or DESCENDING
+    public enum direcaoOrdenacao {
+
+        ASCENDING, DESCENDING
+    }
+    // default sorting would be ascending
+    public static campoOrdenacao CAMPO_ORDENACAO = campoOrdenacao.nome;
+    // default sorting would be on name
+    public static direcaoOrdenacao DIRECAO_ORDENACAO = direcaoOrdenacao.ASCENDING;
 
     /**
      * @return the Nome
@@ -91,13 +104,6 @@ public class Cliente implements Comparable{
         this.Endereco = Endereço;
     }
 
-
-    @Override
-    public int compareTo(Object o) {
-        Cliente c = (Cliente) o;
-       return  (int) (c.getSalario() - this.getSalario());
-    }
-
     /**
      * @return the salario
      */
@@ -125,5 +131,30 @@ public class Cliente implements Comparable{
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
-   
+
+    @Override
+    public int compareTo(Cliente o) {
+        if (CAMPO_ORDENACAO == campoOrdenacao.nome) {
+            if (DIRECAO_ORDENACAO == direcaoOrdenacao.ASCENDING) {
+                return this.Nome.compareTo(o.Nome);
+            } else {
+                return o.Nome.compareTo(this.Nome);
+            }
+
+        } else if (CAMPO_ORDENACAO == campoOrdenacao.sobrenome) {
+                   if (DIRECAO_ORDENACAO == direcaoOrdenacao.ASCENDING) {
+                return this.sobrenome.compareTo(o.sobrenome);
+            } else {
+                return o.sobrenome.compareTo(this.sobrenome);
+            }
+        } else if (CAMPO_ORDENACAO == campoOrdenacao.salario) {
+                   if (DIRECAO_ORDENACAO == direcaoOrdenacao.ASCENDING) {
+                return (int) (this.salario - o.salario);
+            } else {
+                return (int) (o.salario - this.salario);
+            }
+        }
+
+        return 0;
+    }
 }
